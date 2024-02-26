b4c9d7bfe81e67c63e9a9b6ff3b0481f252e78db
Spas Arnaudov <spasarnaudov@gmail.com>
2024-02-26 22:29:22 +0200

    Moved equations setting into single bottom sheet

--- app/src/main/java/com/mathequations/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/mathequations/presentation/ui/MainActivity.kt
@@ -10,28 +10,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.mathequations.R
 import com.mathequations.presentation.math_equation_list.MathEquationsViewModel
 import com.mathequations.domain.equations.formats.EquationItemFormatter
-import com.core.presentation.model.fab.FabItem
 import com.mathequations.presentation.math_equation_list.MathEquationsEvent
 import com.mathequations.presentation.math_equation_list.components.EquationItemsList
-import com.core.presentation.components.fab.ExpandableFab
 import com.mathequations.presentation.math_equation_list.components.FilterCheckList
 import com.mathequations.presentation.math_equation_list.components.IntervalPicker
 import com.mathequations.presentation.math_equation_list.components.OperationPicker
@@ -44,60 +43,30 @@ class MainActivity : ComponentActivity() {
     private val viewModel by viewModels<MathEquationsViewModel>()
     private var showBottomSheet by mutableStateOf(false)
 
-    enum class Fab(val value: Int) {
-        INTERVAL(2),
-        FILTERS(1),
-        OPERATIONS(0),
-        NONE(-1),
-    }
-
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "UnrememberedMutableState")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MathEquationsTheme {
-                val (expanded, setExpanded) = remember { mutableStateOf(false) }
-                var fabId by remember { mutableIntStateOf(Fab.NONE.value) }
                 Surface(
-                    modifier = Modifier.fillMaxSize()
-                        .pointerInput(Unit) {
-                            detectTapGestures(
-                                onPress = {
-                                    // Collapse FABs when pressing outside
-                                    setExpanded(false)
-                                }
-                            )
-                        },
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Scaffold(
                         floatingActionButton = {
-                            ExpandableFab(
-                                fabItems = listOf(
-                                    FabItem(id = Fab.INTERVAL.value, text = getString(R.string.interval)),
-                                    FabItem(id = Fab.FILTERS.value, text = getString(R.string.filters)),
-                                    FabItem(id = Fab.OPERATIONS.value, text = getString(R.string.operations)),
-                                ),
-                                expanded = expanded,
-                                setExpanded = setExpanded,
-                                onFABClick = {
-                                    fabId = it
-                                    showBottomSheet = true
-                                }
-                            )
+                            FloatingActionButton(
+                                onClick = { showBottomSheet = true }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.Settings,
+                                    contentDescription = ""
+                                )
+                            }
                         },
                     ) {
                         Column(
-                            modifier = Modifier.fillMaxSize()
-                                .pointerInput(Unit) {
-                                    detectTapGestures(
-                                        onPress = {
-                                            // Collapse FABs when pressing outside
-                                            setExpanded(false)
-                                        }
-                                    )
-                                },
+                            modifier = Modifier.fillMaxSize(),
                         ) {
                             Text(
                                 modifier = Modifier
@@ -115,20 +84,17 @@ class MainActivity : ComponentActivity() {
                         }
                         if (showBottomSheet) {
                             ModalBottomSheet(
+                                dragHandle = {},
                                 onDismissRequest = {
                                     showBottomSheet = false
                                 },
                             ) {
-                                when (fabId) {
-                                    Fab.INTERVAL.value -> { IntervalPicker() }
-                                    Fab.FILTERS.value -> {
-                                        FilterCheckList(
-                                            filters = viewModel.state.value.mathOperation.filters,
-                                            selectedFilters = viewModel.state.value.filters,
-                                        ) { viewModel.onEvent(MathEquationsEvent.SelectFilter(it)) }
-                                    }
-                                    Fab.OPERATIONS.value -> { OperationPicker() }
-                                }
+                                FilterCheckList(
+                                    filters = viewModel.state.value.mathOperation.filters,
+                                    selectedFilters = viewModel.state.value.filters,
+                                ) { viewModel.onEvent(MathEquationsEvent.SelectFilter(it)) }
+                                OperationPicker()
+                                IntervalPicker()
                             }
                         }
                     }
